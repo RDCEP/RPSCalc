36ea9733586e6007bedabd892ff5aef7d18d4796
Nathan Matteson <matteson@obstructures.org>
2013-11-01 13:47:18 -0500

    Add grid lines, y axis, suppress 2013

--- static/js/trajectory.js
+++ static/js/trajectory.js
@@ -5,7 +5,7 @@ var Trajectory = function() {
     padding = {top: 30, right:30, bottom:30, left:30},
     domain_x = [new Date(2013, 0, 1), new Date(2030, 0, 1)],
     domain_y = [0, 50],
-    adjust_data = {step:.005, start:0, stop:0, index:0},
+    adjust_data = {step:.5, start:0, stop:0, index:0},
     adjust_dot,
     trajectory_drag = d3.behavior.drag()
       .on('drag', drag_move)
@@ -16,6 +16,9 @@ var Trajectory = function() {
       .append('svg')
       .attr('width', width+padding.left+padding.right)
       .attr('height', height+padding.top+padding.bottom),
+    grid_layer = svg.append('g')
+      .attr('id', 'grid_layer')
+      .attr('transform', 'translate('+padding.left+','+padding.top+')'),
     graph = svg.append('g')
       .attr('id', 'graph_layer')
       .attr('transform', 'translate('+padding.left+','+padding.top+')'),
@@ -38,14 +41,14 @@ var Trajectory = function() {
     x = d3.time.scale().domain(domain_x).range([0, width]),
     y = d3.scale.linear().domain(domain_y).range([height, 0]),
     x_axis = d3.svg.axis().scale(x).orient('bottom'),
-    y_axis = d3.svg.axis().scale(y),
+    y_axis = d3.svg.axis().scale(y).orient('left'),
     trajectory_line = d3.svg.line()
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.data); }),
     trajectory_area = d3.svg.area()
       .x(function(d) { return x(d.date); })
       .y0(function(d) { return y(0); })
-      .y1(function(d) { return y(d.data * 100); }),
+      .y1(function(d) { return y(d.data); }),
     time_period_width = x(new Date(2013, 0, 1)) - x(new Date(2012, 0, 1)),
     trajectory,
     handles,
@@ -53,19 +56,19 @@ var Trajectory = function() {
   ;
 
   function drag_move(d, i, ii) {
-    var adjusted = y.invert(d3.event.y) / 100,
+    var adjusted = y.invert(d3.event.y),
       delta = Math.round(adjusted / adjust_data.step) * adjust_data.step,
-      rounded = (delta > 0) ? (delta >.5) ? .5 : delta : 0
+      rounded = (delta > 0) ? (delta > 50) ? 50 : delta : 0
     ;
     data.filter(function(_d) { return _d == d; })[0].data = rounded;
     redraw();
   }
 
   function drag_start(d, i, ii, foo) {
-    console.log(d.date);
+    console.log('start');
     adjust_dot = d3.select(this);
     adjust_data.start = d.data;
-    adjust_data.index = i
+    adjust_data.index = i;
     handles.selectAll('rect').on('mouseover', function() {return null;});
     handles.selectAll('rect').on('mouseout', function() {return null;});
     adjust_dot.on('mouseover', function() {return null;});
@@ -73,8 +76,10 @@ var Trajectory = function() {
   }
 
   function drag_end(d, i, ii) {
+    console.log('end');
     adjust_data.stop = data.filter(function(_d) { return _d == d; })[0].data;
     adjust_dot = null;
+    redraw();
   }
 
   function redraw() {
@@ -82,7 +87,7 @@ var Trajectory = function() {
       .attr('d', trajectory_area(data))
     ;
     inputs.data(data).each(function(d) {
-      d3.select(this).attr('value', function(d) {return d.data * 100;})
+      d3.select(this).attr('value', function(d) {return d.data;})
     });
     handles.data(data)
       .each(function(d,i) {
@@ -98,7 +103,7 @@ var Trajectory = function() {
               .classed('active', false);
           })
         d3.select(this).select('.data-point')
-          .attr('cy', function() {return y(d.data*100);})
+          .attr('cy', function() {return y(d.data);})
           .on('mouseover', function() {
             d3.select(this).classed('active', true).classed('hovered', true);
           })
@@ -107,11 +112,13 @@ var Trajectory = function() {
           })
         ;
         d3.select(this).select('.time-period-label-bkgd')
-          .attr('y', y(d.data*100) - 40)
+          .attr('y', y(d.data) - 40)
         ;
         d3.select(this).select('.time-period-label-text')
-          .text(d3.format('.1f')(d.data*100))
-          .attr('y', y(d.data*100) - 22)
+          .text(function(d) {
+            return (d.date.getFullYear() > 2013) ? d3.format('.1f')(d.data) : null;
+          })
+          .attr('y', y(d.data) - 22)
         ;
       })
     ;
@@ -129,9 +136,7 @@ var Trajectory = function() {
       .enter()
       .append('g')
       .attr('class', 'time-period')
-      .attr('transform', function(d, i) {
-        return 'translate('+(x(d.date)-time_period_width/2)+',0)';
-      })
+      .attr('transform', function(d, i) {return 'translate('+(x(d.date)-time_period_width/2)+',0)';})
     ;
     handles.each(function(d, i) {
       d3.select(this).append('rect')
@@ -142,11 +147,12 @@ var Trajectory = function() {
           return print_date(d.date);
         })
         .style('fill', 'transparent')
+        .style('pointer-events', function(d) {return (d.date.getFullYear() > 2013) ? 'all' : 'none'; })
       ;
       d3.select(this).append('circle')
         .classed('data-point', true)
         .attr('cx', time_period_width/2)
-        .attr('cy', function() {return y(d.data*100);})
+        .attr('cy', function() {return y(d.data);})
         .attr('r', 6)
         .call(trajectory_drag, i)
       ;
@@ -155,7 +161,7 @@ var Trajectory = function() {
         .attr('height', 20)
         .attr('width', time_period_width-4)
         .attr('x', 2)
-        .attr('y', y(d.data*100) - 40)
+        .attr('y', y(d.data) - 40)
         .attr('data-year', function(d) {
           return print_date(d.date);
         })
@@ -164,12 +170,21 @@ var Trajectory = function() {
       d3.select(this).append('text')
         .attr('class', 'time-period-label-text')
         .attr('x', time_period_width/2)
-        .attr('y', y(d.data*100) - 22)
+        .attr('y', y(d.data) - 22)
         .attr('data-year', function(d) {
           return print_date(d.date);
         })
         .style('text-anchor', 'middle')
     });
+    grid_layer.selectAll('.grid-line')
+      .data(y.ticks()).enter()
+      .append('line')
+      .attr('class', 'grid-line')
+      .attr('x1', 0)
+      .attr('x2', x.range()[1])
+      .attr('y1', function(d) { return y(d); })
+      .attr('y2', function(d) { return y(d); })
+    ;
     mask.append('rect').attr('class', 'mask')
       .attr('width', width+padding.left+padding.right).attr('height', padding.top)
       .attr('transform', 'translate(0,0)');
@@ -183,19 +198,22 @@ var Trajectory = function() {
       .attr('width', padding.left).attr('height', height+padding.top+padding.bottom)
       .attr('transform', 'translate('+(width+padding.left)+',0)');
     axes.append('g')
-      .attr('transform', 'translate(0,'+(height)+')')
+      .attr('transform', 'translate(0,'+(height+10)+')')
       .call(x_axis);
+    axes.append('g')
+      .attr('transform', 'translate('+(0)+',0)')
+      .call(y_axis);
     inputs = form.selectAll('input')
       .data(data).enter()
       .append('input').attr('type', 'text')
       .style('width', (time_period_width-14)+'px')
       .style('display', function(d) {return (d.date.getFullYear() > 2013) ? 'block' : 'none'; })
       .on('change', function(d, i) {
-//        console.log(d, i, d3.select(this).property('value'));
-        data.filter(function(_d) { return _d == d; })[0].data = d3.select(this).property('value') / 100;
+        data.filter(function(_d) { return _d == d; })[0].data = d3.select(this).property('value');
         redraw();
       })
     ;
+    //TODO: Below is totally repetitive. Rewrite.
     var drag_switch = d3.select('#drag_switch'),
       input_switch = d3.select('#input_switch');
     drag_switch.on('click', function() {
@@ -228,7 +246,7 @@ var Trajectory = function() {
         if (d.properties.trajectory) {
           _date_data = [];
           d.properties.trajectory.forEach(function(dd, ii) {
-            _date_data.push({'data': dd, 'date': parse_date(String(ii+ 2000))});
+            _date_data.push({'data': dd*100, 'date': parse_date(String(ii+ 2000))});
           });
           d.properties.trajectory = _date_data;
         }
