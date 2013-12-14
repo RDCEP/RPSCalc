2bacf9b36fe332788a525d15560f71a23b70628d
Nathan Matteson <matteson@obstructures.org>
2013-12-13 23:37:35 -0600

    Blueprints for EIA API and rpscalc

--- rpscalc/__init__.py
+++ rpscalc/__init__.py
@@ -11,10 +11,12 @@ app.url_map.strict_slashes = False
 def not_found(error):
     return render_template('errors/404.html'), 404
 
-mod = Blueprint('rpscalc', __name__, static_folder='static')
-
+# Load and register Blueprints
 from rpscalc.state_pages.views import mod as state_pages_module
 from rpscalc.calculator.views import mod as calculator_module
+from rpscalc.eia_api.views import mod as eia_module
+from rpscalc.views import mod as rpscalc_module
 app.register_blueprint(state_pages_module)
 app.register_blueprint(calculator_module)
-app.register_blueprint(mod)
+app.register_blueprint(rpscalc_module)
+app.register_blueprint(eia_module)
