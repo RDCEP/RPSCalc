81cd91dd8e54c86e5df7ec2c9fd91919378c0b70
Nathan Matteson <matteson@obstructures.org>
2014-01-14 12:48:45 -0600

    Initial commit of filters

---
+++ rpscalc/filters.py
@@ -0,0 +1,12 @@
+import json
+
+
+def session_json(_session):
+    return json.dumps({ k: v for k, v in _session.iteritems()})
+
+def session_cleared(_session):
+    _s = { k: v for k, v in _session.iteritems()}
+    for k in ['trajectory', 'wind', 'solar', 'rps', 'price_and_policy']:
+        if k in _s.keys():
+            _s[k] = False
+    return json.dumps(_s)
