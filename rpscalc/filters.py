e8b702f5056dee6627696448763c30b811b14e4b
Nathan Matteson <matteson@obstructures.org>
2014-01-15 07:06:32 -0600

    Fixes for session updates

--- rpscalc/filters.py
+++ rpscalc/filters.py
@@ -4,6 +4,7 @@ import json
 def session_json(_session):
     return json.dumps({ k: v for k, v in _session.iteritems()})
 
+
 def session_cleared(_session):
     _s = { k: v for k, v in _session.iteritems()}
     for k in ['trajectory', 'wind', 'solar', 'rps', 'price_and_policy']:
