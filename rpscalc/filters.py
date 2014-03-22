d999d5393ef5920a6bfea5bd7f545a47fe722385
Nathan Matteson <matteson@obstructures.org>
2014-03-22 13:53:39 -0500

    PEP8

--- rpscalc/filters.py
+++ rpscalc/filters.py
@@ -2,11 +2,11 @@ import json
 
 
 def session_json(_session):
-    return json.dumps({ k: v for k, v in _session.iteritems()})
+    return json.dumps({k: v for k, v in _session.iteritems()})
 
 
 def session_cleared(_session):
-    _s = { k: v for k, v in _session.iteritems()}
+    _s = {k: v for k, v in _session.iteritems()}
     for k in ['trajectory', 'wind', 'solar', 'rps', 'price_and_policy']:
         if k in _s.keys():
             _s[k] = False
