d36486b06c0cab24fd543e837c26f6374c3216ab
Nathan Matteson <matteson@obstructures.org>
2014-04-11 10:23:30 -0500

    Update PA overview

--- rpscalc/state_pages/views.py
+++ rpscalc/state_pages/views.py
@@ -5,6 +5,7 @@ State page overviews for RPS Calculator.
 """
 
 from flask import Blueprint, render_template, session, abort
+from rpscalc.constants import RPS_STATES
 
 
 mod = Blueprint('state_pages', __name__, url_prefix='/state',
@@ -19,5 +20,6 @@ def overview(state):
     return render_template(
         'overview_states/{}.html'.format(state),
         state=state,
+        pretty_state=[s['names'][1] for s in RPS_STATES.values() if s['names'][0] == state][0],
         session_clear='true',
     )
